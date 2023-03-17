d0651d16c0718a8600e9d60e3bb22df6b3b9c1c8
kuangyizheng@outlook.com <kuangyizheng@outlook.com>
2023-03-17 12:13:01 +0800

    feat:增加middlewares文件

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { ProjectListScreem } from 'screens/project-list';
-
+import { LoginScreen } from 'screens/login';
 function App() {
   return (
     <div className="App">
-      <ProjectListScreem />
+      <LoginScreen />
+      {/* <ProjectListScreem /> */}
     </div>
   );
 }
